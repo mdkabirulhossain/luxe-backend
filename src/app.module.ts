4c8918653d91bd553568c23e242059c320c11625
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-06-08 20:26:46 +0600

    All the module is created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaClientModule } from './prisma-client/prisma-client.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { CategoryModule } from './modules/category/category.module';
+import { ProductModule } from './modules/product/product.module';
+import { CartModule } from './modules/cart/cart.module';
+import { WishlistModule } from './modules/wishlist/wishlist.module';
+import { OrderModule } from './modules/order/order.module';
+import { ReviewModule } from './modules/review/review.module';
+import { AdminModule } from './modules/admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaClientModule, AuthModule, UserModule, CategoryModule, ProductModule, CartModule, WishlistModule, OrderModule, ReviewModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
