bb481bdec95a64b96966bfd0924b1c45926322eb
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-06-08 21:46:30 +0600

    remove prisma client warning

--- src/prisma-client/prisma-client.module.ts
+++ src/prisma-client/prisma-client.module.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaClientService } from './prisma-client.service';
