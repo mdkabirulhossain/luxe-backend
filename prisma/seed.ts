9aa3c99589a7727e6e5fa140e708d56b22ae9027
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-06-16 18:33:41 +0600

    Updated seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable prettier/prettier */
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
