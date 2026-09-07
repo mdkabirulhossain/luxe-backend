af77bf5dd1119558878572226c2368cb4be2ab50
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-09-07 16:16:47 +0600

    updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -46,7 +46,12 @@ function loadEnv() {
 
 loadEnv();
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
